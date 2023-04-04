48ee444c019677502dad2c86b08c77c99034f567
David <david.daso@sterling.ng>
2023-04-04 19:15:47 +0100

    data

--- SP/frontend/src/components/App.js
+++ SP/frontend/src/components/App.js
@@ -1,15 +1,18 @@
 import React, {Component} from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
     constructor(props){
         super(props);
     }
-
+    
     render(){
-        return <h1>Hi boss</h1>;
+        return <h1>Hi boss</h1>
+        
+        
     }
 }
 
-const AppDiv = document.getElementById("app")
-render(<App/>, AppDiv)
+const AppDiv = document.getElementById("app");
+render(<App/>, AppDiv);
